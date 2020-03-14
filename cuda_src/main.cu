1a3f628d6197f2d44da349cf4527f0a6eaf35ca0
Ivans <ivnsimakhin@gmail.com>
2020-03-14 20:31:08 +0300

    Added tensors

--- cuda_src/main.cu
+++ cuda_src/main.cu
@@ -6,35 +6,48 @@
 // CATCH_CUDA_ERR(cudaMemcpy(dev_array, array, sizeof(int) * used_n, cudaMemcpyHostToDevice));
 
 int main() {
-    int n = 1000, m = 1000;
-    Matrix* mtr = hilbert(n, m);
+    int n = 3, m = 3, k = 3;
+    // Matrix* mtr = hilbert(n, m);
+    int * shapes = new int[3];
+    shapes[0] = n;
+    shapes[1] = m;
+    shapes[2] = k;
+    Matrix* mtr = hilbert(n, m, k);
+    delete[] shapes;
     // auto p = QRDecompositionNaive(mtr);
     // Matrix* res = multiply(p.first, p.second);
-    auto trip = SVDDecomposition(mtr, 15, 1e-6);
+    // auto trip = SVDDecomposition(mtr, 10, 1e-6);
     // printf("HI");
-    auto tmp = multiply(trip->first, trip->second);
-    auto t = transpose(trip->third);
-    auto res = multiply(tmp, t);
-    printf("Diff: %f \n", diff(mtr, res));
+    // auto tmp = multiply(trip->first, trip->second);
+    // auto t = transpose(trip->third);
+    // auto res = multiply(tmp, t);
+    // printf("Diff: %f \n", diff(mtr, res));
     // printf("Size(%d %d), Diff: %f \n", n, m, diff(mtr, res));
     // Matrix* d_mtr;
     // double* d_mtr_arr;
     // int* d_dims_arr;
     // copyMatrixFromHostToDevice(mtr, &d_mtr, &d_mtr_arr, &d_dims_arr);
     // show(mtr, n, m);
+    // int* newShapes = new int[2];
+    // newShapes[0] = 8;
+    // newShapes[1] = 2;
+    // mtr->reshape(newShapes, 2);
+    // show(mtr, 8, 2);
+
     // show(res, n, m);
+    show(mtr, n, m, k);
     delete mtr;
 
     // printf("%d %d %d\n", d_mtr, d_mtr_arr, d_dims_arr);
     // (cudaFree(d_mtr));
     // (cudaFree(d_mtr_arr));
     // (cudaFree(d_dims_arr));
-    delete tmp;
-    delete t;
-    delete res;
-    delete trip->first;
-    delete trip->second;
-    delete trip->third;
-    delete trip;
+    // delete tmp;
+    // delete t;
+    // delete res;
+    // delete trip->first;
+    // delete trip->second;
+    // delete trip->third;
+    // delete trip;
     return 0;
 }
--- cuda_src/matrix/Matrix.cuh
+++ cuda_src/matrix/Matrix.cuh
@@ -27,15 +27,16 @@ public:
         }
     }
 
-    Matrix(int dims_count, int* dims_value, int fake) {
+    Matrix(int dims_count, int* dims_value): dims_count(dims_count) {
         int total = 1;
+        dims = new int[dims_count];
         for (int i = 0; i < dims_count; i++) {
             total *= dims_value[i];
+            dims[i] = dims_value[i];
         }
         matrix = new double[total];
-        dims = new int[total];
         for (int i = 0; i < total; i++) {
-            dims[i] = 0;
+            matrix[i] = 0;
         }
     }
 
@@ -48,23 +49,76 @@ public:
 
     __host__ __device__
     double get(int i, int j) {
+        if (dims_count != 2) {
+            printf("Wrong sized, cur: %d, but required 2\n", dims_count);
+            return 0;
+        }
+        // i + n * j
         int index = i + dims[0] * j;
         return matrix[index];
     }
 
     __host__ __device__
     void set(int i, int j, double value) {
+        // i + n * j
+        if (dims_count != 2) {
+            printf("Wrong sized, cur: %d, but required 2\n", dims_count);
+            return;
+        }
         int index = i + dims[0] * j;
         matrix[index] = value;
     }
 
     __host__
-    void reshape(int* new_dims, int new_dim_count) {
+    double get(int* indexes, int index_count) {
+        if (index_count != dims_count) {
+            printf("Wrong sized (cur: %d, passed: %d)\n", dims_count, index_count);
+            return 0;
+        }
+        // for dim_count = 3
+        // index for i j k
+        // index = i + dims[0] * j + dims[0] * dims[1] * k;
+        // int index = i;
+        int index = 0;
+        int mlt = 1;
+        for (int i = 0; i < dims_count; i++) {
+            index += indexes[i] * mlt;
+            if (i != dims_count - 1) {
+                mlt *= dims[i];
+            }
+        }
+        return matrix[index];
+    }
+
+    __host__
+    void set(int* indexes, int index_count, double value) {
+        if (index_count != dims_count) {
+            printf("Wrong sized (cur: %d, passed: %d)\n", dims_count, index_count);
+            return;
+        }
+        // for dim_count = 3
+        // index for i j k
+        // index = i + dims[0] * j + dims[0] * dims[1] * k;
+        // int index = i;
+        int index = 0;
+        int mlt = 1;
+        for (int i = 0; i < dims_count; i++) {
+            index += indexes[i] * mlt;
+            if (i != dims_count - 1) {
+                mlt *= dims[i];
+            }
+        }
+        printf("index: %d\n", index);
+        matrix[index] = value;
+    }
+
+    __host__
+    void reshape(int* new_dims, int new_dims_count) {
         delete[] dims;
         
-        dims_count = new_dim_count;
+        dims_count = new_dims_count;
         dims = new int[dims_count];
-        for (int i = 0; i < new_dim_count; i++) {
+        for (int i = 0; i < new_dims_count; i++) {
             dims[i] = new_dims[i];
         }
     };
